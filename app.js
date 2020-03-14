ffcc015e81d4b24c92f95deecd5bf2b040fcfa09
Anton <anton.grigorenko91@gmail.com>
2020-03-14 21:10:51 +0200

    Added for loop

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 console.log("Hello world");
-
+const array = [1, 2, 3, 4, 5, 6];
 console.log("Hello to you too, hooman!");
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+}
